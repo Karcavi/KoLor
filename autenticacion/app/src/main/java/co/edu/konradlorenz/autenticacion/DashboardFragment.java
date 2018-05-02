81f94a86c6dccfca2089ec0c77e6daddd9b77f4e
IAN-PC\IAN <sebastianmendez1323@gmail.com>
2018-05-01 20:30:24 -0500

    *List view on HomeFragment *New strings values in English *New button on ProfileFragment to edit a profile

--- autenticacion/app/src/main/java/co/edu/konradlorenz/autenticacion/DashboardFragment.java
+++ autenticacion/app/src/main/java/co/edu/konradlorenz/autenticacion/DashboardFragment.java
@@ -7,13 +7,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 
 /**
@@ -23,6 +33,15 @@ public class DashboardFragment extends Fragment {
 
     private EditText Title;
     private EditText Desrcription;
+    // Firebase var
+    private DatabaseReference rootRef;
+    // private DatabaseReference rootRef1;
+    // Android Layout
+    private ListView listView;
+    private FirebaseAuth mAuth;
+    //Array List
+    private ArrayList<PublicationEntity> arrayList = new ArrayList<PublicationEntity>();
+    private ArrayAdapter<String> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +61,45 @@ public class DashboardFragment extends Fragment {
 
         Title = (EditText) view.findViewById(R.id.etxt_name);
         Desrcription = (EditText) view.findViewById(R.id.etxt_lastname);
+        adapter = new ArrayAdapter(getActivity(),
+                android.R.layout.activity_list_item, android.R.id.text1);
+
+        listView = (ListView) view.findViewById(R.id.listViewD);
+        listView.setAdapter(adapter);
+        //Firebase
+        rootRef = FirebaseDatabase.getInstance().getReference("publications");
+        //rootRef1 = FirebaseDatabase.getInstance().getReference("users");
+        //read adds
+        rootRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                PublicationEntity resultado = dataSnapshot.getValue(PublicationEntity.class);
+                UserEntity resultado1 = dataSnapshot.getValue(UserEntity.class);
+                adapter.add(resultado.getTitle());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG:", "Failed to read value.", error.toException());
+            }
+        });
         return view;
     }
 
